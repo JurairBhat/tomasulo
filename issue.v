c11ca474553b3aa4db2dcfa1a7a79092d97f8063
JurairBhat <64773905+JurairBhat@users.noreply.github.com>
2020-05-04 13:49:39 +0530

    Add files via upload

---
+++ issue.v
@@ -0,0 +1,18 @@
+/* This module gets the instruction from the memory
+puts the data in instruction register */
+module fetch(
+  input [7:0] mem_address,
+  input mem_read ,
+  input ir_w , // it only write in intruction register if ir_w = 1
+  output [31:0] opcode
+  );
+
+  // code to put
+endmodule
+
+module(
+    input [31:0]
+
+  )
+
+  )
